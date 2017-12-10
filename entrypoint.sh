8c5fe9d71a8acabf907b5d612b3e7948dc7d5b30
yulahuyed <yulahuyed@vpsorg.top>
2017-12-10 20:21:59 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,12 @@
 cd /v2raybin
-echo -e -n "$CONFIG_JSON1" > config.json
-echo -e -n "$UUID" >> config.json
-echo -e -n "$CONFIG_JSON2" >> config.json
+if [ "${CONFIG}" ]
+then
+    echo -e "${CONFIG}" > config.json
+else
+    echo -e -n "$CONFIG_JSON1" > config.json
+    echo -e -n "$UUID" >> config.json
+    echo -e -n "$CONFIG_JSON2" >> config.json
+fi
 if [ "$CERT_PEM" != "$KEY_PEM" ]; then
 echo -e "$CERT_PEM" > cert.pem
 echo -e "$KEY_PEM"  > key.pem
