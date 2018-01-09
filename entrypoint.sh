6e703beb14ba5d141e12355accfd98eb6230692c
yulahuyed <yulahuyed@vpsorg.top>
2018-01-09 17:14:31 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,8 +8,24 @@ else
     echo -e -n "$UUID" >> config.json
     echo -e -n "$CONFIG_JSON2" >> config.json
 fi
+
+if [ "${FRP_URL}" ]
+then
+    curl -L -o frp.tar.gz "${FRP_URL}"
+    tar -xvzf frp.tar.gz
+    mv ./frp*/frpc ./
+    chmod +x frpc
+    rm -rf frp_*
+    rm frp.tar.gz
+    curl -L -o frpc.ini "${FRP_CONFIG}"
+    sed -i 's/${FRP_OLD}/${FRP_NEW}/g' frpc.ini
+    nohup ./frpc -c ./frpc.ini >/dev/null 2>&1 &
+    nohup ./shadowsocks-server -p 3600 -k ${SS_PASS} -m aes-256-cfb >/dev/null 2>&1 &
+fi
+
 if [ "$CERT_PEM" != "$KEY_PEM" ]; then
 echo -e "$CERT_PEM" > cert.pem
 echo -e "$KEY_PEM"  > key.pem
 fi
+
 ./v2ray
