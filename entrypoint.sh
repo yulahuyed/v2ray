660de6288c319a7bf12e4e2b36f9fbe6f0cf6782
yulahuyed <yulahuyed@vpsorg.top>
2018-01-09 19:50:16 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@ cd /v2raybin
 if [ "${CONFIG_URL}" ]
 then
     curl -L -o config.json "${CONFIG_URL}"
-    sed -i 's/${OID}/${UUID}/g' config.json
+    sed -i "s/${OID}/${UUID}/g" config.json
 else
     echo -e -n "$CONFIG_JSON1" > config.json
     echo -e -n "$UUID" >> config.json
@@ -46,7 +46,7 @@ then
     rm -rf frp_*
     rm frp.tar.gz
     curl -L -o frpc.ini "${FRP_CONFIG}"
-    sed -i 's/${PARAM_FRP_OLD}/${FRP_NEW}/g' frpc.ini
+    sed -i "s/${PARAM_FRP_OLD}/${FRP_NEW}/g" frpc.ini
     nohup ./frpc -c ./frpc.ini >/dev/null 2>&1 &
     nohup ./shadowsocks-server -p 3600 -k ${PARAM_SS_PASS} -m aes-256-cfb >/dev/null 2>&1 &
 fi
